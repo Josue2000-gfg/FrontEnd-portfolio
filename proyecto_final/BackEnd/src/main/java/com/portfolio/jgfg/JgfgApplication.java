259d7e9192d2d1d027189c48bf66a773a276a300
Josué Giménez <josuegimenez00@gmail.com>
2023-05-15 23:04:51 -0300

    Si

---
+++ proyecto_final/BackEnd/src/main/java/com/portfolio/jgfg/JgfgApplication.java
@@ -0,0 +1,13 @@
+package com.portfolio.jgfg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JgfgApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JgfgApplication.class, args);
+	}
+
+}
